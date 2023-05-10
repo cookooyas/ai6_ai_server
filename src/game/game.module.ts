7b3ef8504e9278fa1bcadf30baa10faa744ca6b7
hj6elice <gZMz9ZbeZQyxE@F>
2023-05-10 14:41:20 +0900

    feat: game score rank api

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   controllers: [GameController],
-  providers: [GameService]
+  providers: [GameService],
+  imports: [PrismaModule],
 })
 export class GameModule {}
---
+++ src/util/swaggerOkResponse/getGameRankList.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class GetGameRankList {
+  @ApiProperty({ example: 1 })
+  id: number;
+
+  @ApiProperty({ example: '모모모' })
+  nickname: string;
+
+  @ApiProperty({ example: 'https://www.~~~' })
+  profile_image_url: number;
+
+  @ApiProperty({ example: 100 })
+  score: number;
+
+  @ApiProperty({ example: 3 })
+  rank: number;
+}
