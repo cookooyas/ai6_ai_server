a9c5bac27da56fa2d348e371cd66c90bd3d0d3dc
기석 이 <cookooyas@gmail.com>
2023-05-14 16:20:22 +0900

    feature: user 개선 및 수정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -23,6 +23,8 @@ import {
   ApiProperty,
 } from '@nestjs/swagger';
 import { GetUserProfile } from 'src/util/swaggerOkResponse/getUserProfile';
+import { UpdateUserProfile } from 'src/util/swaggerOkResponse/updateUserProfile';
+import { GetUserCalendar } from 'src/util/swaggerOkResponse/getUserCalendar';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('user')
@@ -45,21 +47,52 @@ export class UserController {
     return await this.userService.findUser(+user_id);
   }
 
+  @ApiOperation({
+    summary: 'user profile 수정 API',
+    description: '유저토큰을 통해 인증하고 바디 정보로 회원정보를 수정한다.',
+  })
+  @ApiBody({ type: UpdateUserInfoDto })
+  @ApiOkResponse({
+    status: 200,
+    description: '정상 응답 (수정된 메세지를 반환한다)',
+    type: UpdateUserProfile,
+  })
   @Patch('/profile')
   async updateUser(@Req() req, @Body() updateUserInfoDto: UpdateUserInfoDto) {
     const { user_id } = req.user;
     return await this.userService.updateUser(+user_id, updateUserInfoDto);
   }
 
+  @ApiOperation({
+    summary: 'user profile 수정 API',
+    description: '유저토큰을 통해 인증하고 바디 정보로 회원정보를 수정한다.',
+  })
+  @ApiBody({ type: UpdatePasswordDto })
+  @ApiOkResponse({
+    status: 200,
+    description: '정상 응답 (request success메세지를 반환한다.)',
+    type: 'request success',
+  })
   @Patch('/password')
   async updatePassword(
     @Req() req,
     @Body() updatePasswordDto: UpdatePasswordDto,
   ) {
     const { user_id } = req.user;
-    return await this.userService.updatePassword(+user_id, updatePasswordDto);
+    await this.userService.updatePassword(+user_id, updatePasswordDto);
+    return 'request success';
   }
 
+  @ApiOperation({
+    summary: 'user calender 조회 API',
+    description: '유저토큰을 통해 유저의 일일 플레이 기록을 조회한다.',
+  })
+  @ApiQuery({ name: 'year', required: true, description: '조회 년도 (4자리)' })
+  @ApiQuery({ name: 'month', required: true, description: '조회 월 (1~2자리)' })
+  @ApiOkResponse({
+    status: 200,
+    description: '정상 응답 (매일 플레이 기록을 반환한다.)',
+  })
   @Get('/calendar')
   async findCalendar(
     @Req() req,
@@ -70,18 +103,33 @@ export class UserController {
     return await this.userService.findCalendar(+user_id, year, month);
   }
 
+  @ApiOperation({
+    summary: 'user likes 조회 API',
+    description: '유저토큰을 통해 인증하고 likes 목록을 조회한다.',
+  })
   @Get('/likes')
   async findLikes(@Req() req, @Query('pageno') pageno: string) {
     const { user_id } = req.user;
-    return await this.userService.findLikes(+user_id, +pageno);
+    return await this.userService.findLikes(+user_id, pageno ? +pageno : 0);
   }
 
+  @ApiOperation({
+    summary: 'user profile 수정 API',
+    description: '유저토큰을 통해 인증하고 바디 정보로 회원정보를 수정한다.',
+  })
   @Get('/game/history')
   async findAllGameHistory(@Req() req, @Query('pageno') pageno: string) {
     const { user_id } = req.user;
-    return await this.userService.findAllGameHistory(+user_id, +pageno);
+    return await this.userService.findAllGameHistory(
+      +user_id,
+      pageno ? +pageno : 0,
+    );
   }
 
+  @ApiOperation({
+    summary: 'user profile 수정 API',
+    description: '유저토큰을 통해 인증하고 바디 정보로 회원정보를 수정한다.',
+  })
   @Get('/game/history/:musicId')
   async findOneGameHistory(@Req() req, @Param('musicId') musicId: string) {
     const { user_id } = req.user;
---
+++ src/util/swaggerOkResponse/updateUserProfile.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateUserProfile {
+  @ApiProperty({ example: 16 })
+  id: number;
+
+  @ApiProperty({ example: 16 })
+  user_id: number;
+
+  @ApiProperty({ example: 'test3' })
+  nickname: string;
+
+  @ApiProperty({
+    example:
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fgithub.com%2Fscottsweb%2Fnull&psig=AOvVaw3BsOhYVRqYg2iiMss_NVsU&ust=1683609303786000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPCGp9n75P4CFQAAAAAdAAAAABAJ',
+  })
+  profile_image_url: string;
+
+  @ApiProperty({ example: 1 })
+  current_tier: number;
+}
