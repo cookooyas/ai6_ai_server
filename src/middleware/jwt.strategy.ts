4a6789296bd1e7a05bc4ee918808ba675debaaa3
cookooyas <cookooyas@gmail.com>
2023-05-15 09:48:06 +0900

    feature:auth기능추가 및 개선

--- src/middleware/jwt.strategy.ts
+++ src/middleware/jwt.strategy.ts
@@ -30,15 +30,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
   async validate(payload) {
-    const { email } = payload;
+    const { user_id } = payload;
 
-    const user_auth = await this.prismaService.user_auth.findFirst({
-      where: { email },
-    });
-    if (!user_auth) {
-      throw new UnauthorizedException();
-    }
-    const { user_id } = user_auth;
     const { deleted_at } = await this.prismaService.user.findUnique({
       where: { id: user_id },
     });
@@ -49,9 +42,20 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       where: { user_id },
     });
     const { expired_at } = await this.prismaService.user_token.findFirst({
-      where: { user_id: user_id },
+      where: { user_id },
     });
     const isAdmin = admin ? true : false;
-    return { user_id, isAdmin, expired_at };
+
+    // 여기서 유저 토큰을 연장시켜주는 방법도 고려해볼만하다. 현재시간이 expired_at보다 전이고, 시간이 10분이내로 남는다면 재발급해주는 방법이 괜찮을듯 하다 커스텀 데코레이터를 만들어서 expired_in이 true일 경우에 어떠한 로직을 처리하게끔하자.
+    const date = new Date();
+    let expired_in = false;
+    if (date < expired_at) {
+      date.setSeconds(date.getDate() + 60 * 60);
+      if (date > expired_at) {
+        console.log('this token will be expired in 10 min');
+        expired_in = true;
+      }
+    }
+    return { user_id, isAdmin, expired_in };
   }
 }
