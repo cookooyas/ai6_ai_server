d499f21cd0fb5ee770aeb3cf6fe658ed0dc9545c
hj6elice <gZMz9ZbeZQyxE@F>
2023-05-18 11:26:07 +0900

    refactor: genre service

--- src/music/genre/genre.service.ts
+++ src/music/genre/genre.service.ts
@@ -3,12 +3,23 @@ import { PrismaService } from '../../prisma/prisma.service';
 import { UpdateGenreDto } from '../../dto/update-genre.dto';
 import { CreateGenreDto } from '../../dto/create-genre.dto';
 import { GetGenreInfoDto } from '../../dto/get-genre-info.dto';
+import { music_genre } from '@prisma/client';
 
 @Injectable()
 export class GenreService {
   constructor(private prisma: PrismaService) {}
 
-  async getAll() {
+  async getByName(name): Promise<void> {
+    const found: music_genre = await this.prisma.music_genre.findUnique({
+      where: { name },
+    });
+    if (!found) {
+      throw new NotFoundException('이미 사용 중인 이름입니다.');
+    }
+    return;
+  }
+
+  async getAll(): Promise<GetGenreInfoDto[]> {
     const genres: GetGenreInfoDto[] = await this.prisma.music_genre.findMany({
       select: {
         id: true,
@@ -18,7 +29,7 @@ export class GenreService {
     return genres;
   }
 
-  async getOne(id: number) {
+  async getOne(id: number): Promise<GetGenreInfoDto> {
     const genre: GetGenreInfoDto = await this.prisma.music_genre.findFirst({
       where: { id },
       select: {
@@ -26,20 +37,21 @@ export class GenreService {
         name: true,
       },
     });
-    // 에러 처리 나중에 미들웨어로 구현, 에러코드표도 작성하기
     if (!genre) {
       throw new NotFoundException('장르 정보를 찾을 수 없습니다.');
     }
     return genre;
   }
 
-  async create(genreData: CreateGenreDto) {
+  async create(genreData: CreateGenreDto): Promise<void> {
+    await this.getByName(genreData.name);
     await this.prisma.music_genre.create({ data: genreData });
     return;
   }
 
-  async patch(id: number, updateData: UpdateGenreDto) {
+  async patch(id: number, updateData: UpdateGenreDto): Promise<void> {
     await this.getOne(id);
+    await this.getByName(updateData.name);
     await this.prisma.music_genre.update({
       where: { id },
       data: updateData,
@@ -47,7 +59,7 @@ export class GenreService {
     return;
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<void> {
     await this.getOne(id);
     await this.prisma.music_genre.delete({ where: { id } });
     return;
