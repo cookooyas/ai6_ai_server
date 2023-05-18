2e6bc8dd04433c7445fcbc13a09edeaf86d9bbad
hj6elice <gZMz9ZbeZQyxE@F>
2023-05-18 15:12:03 +0900

    refactor: singer api

--- src/music/singer/singer.service.ts
+++ src/music/singer/singer.service.ts
@@ -1,20 +1,24 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateSingerDto } from '../../dto/create-singer.dto';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UpdateSingerDto } from '../../dto/update-singer.dto';
 import { GetSingerInfoDto } from '../../dto/get-singer-info-dto';
 import { music_singer } from '@prisma/client';
+import { ERROR_MESSAGE } from '../../util/error';
 
 @Injectable()
 export class SingerService {
   constructor(private prisma: PrismaService) {}
 
-  async getByName(name): Promise<void> {
+  async isDuplicated(name): Promise<void> {
     const found: music_singer = await this.prisma.music_singer.findUnique({
       where: { name },
     });
-    if (!found) {
-      throw new NotFoundException('이미 사용 중인 이름입니다.');
+    if (found) {
+      throw new HttpException(
+        ERROR_MESSAGE.CONFLICT.SINGER,
+        HttpStatus.CONFLICT,
+      );
     }
     return;
   }
@@ -40,20 +44,23 @@ export class SingerService {
       },
     });
     if (!singer) {
-      throw new NotFoundException('가수 정보를 찾을 수 없습니다.');
+      throw new HttpException(
+        ERROR_MESSAGE.NOT_FOUND.SINGER,
+        HttpStatus.NOT_FOUND,
+      );
     }
     return singer;
   }
 
   async create(singerData: CreateSingerDto): Promise<void> {
-    await this.getByName(singerData.name);
+    await this.isDuplicated(singerData.name);
     await this.prisma.music_singer.create({ data: singerData });
     return;
   }
 
   async patch(id: number, updateData: UpdateSingerDto): Promise<void> {
     await this.getOne(id);
-    await this.getByName(updateData.name);
+    await this.isDuplicated(updateData.name);
     await this.prisma.music_singer.update({
       where: { id },
       data: updateData,
